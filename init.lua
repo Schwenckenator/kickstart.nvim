c78c4d485453c56a9297b462f22783bb0fed8f63
Matthew Schwencke <matt.schwencke@proton.me>
2024-02-08 23:14:25 +0900

    Removed ':' requirement from TODO

--- init.lua
+++ init.lua
@@ -308,7 +308,17 @@ require('lazy').setup({
     dependencies = {
       "nvim-lua/plenary.nvim",
     },
-    opts = {},
+    opts = {
+      highlight = {
+        pattern = [[.*<(KEYWORDS)\s*]], -- pattern or table of patterns, used for highlighting (vim regex)
+        keyword = "bg",
+      },
+      search = {
+        -- regex that will be used to match keywords.
+        -- don't replace the (KEYWORDS) placeholder
+        pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
+      },
+    },
   },
 
 }, {})
