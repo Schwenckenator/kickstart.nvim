2f8a348b2054c58df652118bd936699e1d245b4a
Matthew Schwencke <matt.schwencke@proton.me>
2024-05-30 14:52:21 +0900

    Fixed typescript snippets

--- init.lua
+++ init.lua
@@ -982,5 +982,6 @@ require('lazy').setup {
 -- -- Load custom snippets
 -- require('luasnip.loaders.from_vscode').lazy_load { paths = '~/.config/nvim' }
 
+-- require('luasnip').log.open()
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
