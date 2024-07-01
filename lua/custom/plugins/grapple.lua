8a776fd007c3f4d962fafa8ef23b63c0161c0445
Matthew Schwencke <matt.schwencke@proton.me>
2024-07-01 14:49:55 +0900

    added hotkeys for grapple files up to 9

--- lua/custom/plugins/grapple.lua
+++ lua/custom/plugins/grapple.lua
@@ -16,5 +16,10 @@ return {
     { '<leader>2', '<CMD>Grapple select index=2<CR>', desc = 'Select Tag [2]' },
     { '<leader>3', '<CMD>Grapple select index=3<CR>', desc = 'Select Tag [3]' },
     { '<leader>4', '<CMD>Grapple select index=4<CR>', desc = 'Select Tag [4]' },
+    { '<leader>5', '<CMD>Grapple select index=5<CR>', desc = 'Select Tag [5]' },
+    { '<leader>6', '<CMD>Grapple select index=6<CR>', desc = 'Select Tag [6]' },
+    { '<leader>7', '<CMD>Grapple select index=7<CR>', desc = 'Select Tag [7]' },
+    { '<leader>8', '<CMD>Grapple select index=8<CR>', desc = 'Select Tag [8]' },
+    { '<leader>9', '<CMD>Grapple select index=9<CR>', desc = 'Select Tag [9]' },
   },
 }
