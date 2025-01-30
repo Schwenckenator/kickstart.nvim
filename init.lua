d7717ab9d49b907a73b51a77f3241b140aff936c
Matthew Schwencke <matt.schwencke@proton.me>
2025-01-30 09:09:55 +0900

    Added floating terminal

--- init.lua
+++ init.lua
@@ -345,25 +345,25 @@ require('lazy').setup {
       vim.keymap.set('n', '<leader>?', '<CMD>WhichKey<CR>', { desc = 'Display WhichKey' })
 
       -- Document existing key chains
-      require('which-key').register {
-        ['gj'] = { name = '[J]sdoc comment', _ = 'which_key_ignore' },
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>ci'] = { name = '[I]mports', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>f'] = { name = '[F]ile', _ = 'which_key_ignore' },
-        ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-        ['<leader>v'] = { name = '[V]oid', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        ['<leader>x'] = { name = 'Fi[X]', _ = 'which_key_ignore' },
-        ['<leader><leader>r'] = { name = '[R]eload config', _ = 'which_key_ignore' },
+      require('which-key').add {
+        { 'gj', group = '[J]sdoc comment' },
+        { '<leader>c', group = '[C]ode' },
+        { '<leader>ci', group = '[I]mports' },
+        { '<leader>d', group = '[D]ocument' },
+        { '<leader>f', group = '[F]ile' },
+        { '<leader>g', group = '[G]it' },
+        { '<leader>r', group = '[R]ename' },
+        { '<leader>s', group = '[S]earch' },
+        { '<leader>t', group = '[T]oggle' },
+        { '<leader>v', group = '[V]oid' },
+        { '<leader>w', group = '[W]orkspace' },
+        { '<leader>x', group = 'Fi[X]' },
+        { '<leader><leader>r', group = '[R]eload config' },
       }
 
-      require('which-key').register({
-        ['<leader>'] = { name = 'VISUAL <leader>' },
-        ['<leader>h'] = { 'Git [H]unk' },
+      require('which-key').add({
+        { '<leader>', group = 'VISUAL <leader>' },
+        { '<leader>h', group = 'Git [H]unk' },
       }, { mode = 'v' })
     end,
   },
@@ -605,6 +605,8 @@ require('lazy').setup {
           -- code, if the language server you are using supports them
           if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
             map('<leader>th', function()
+              local enabled = vim.lsp.inlay_hint.is_enabled()
+              print('hints are currently ' .. tostring(enabled))
               vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
             end, '[T]oggle Inlay [H]ints')
 
